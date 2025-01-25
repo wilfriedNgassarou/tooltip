3e0830286b91642472a6b731d8331f94c8daca52
unknown <wilfriedngassarou23@gmail.com>
2025-01-25 12:39:39 +0100

    Added credits

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { AnimatePresence, motion } from "motion/react";
 import { MouseEvent, useRef, useState } from "react"
 import { users } from "./constants/users";
+import { Credits } from "./components/credits";
 
 function App() {
   const prevX = useRef(0);
@@ -18,48 +19,49 @@ function App() {
       onMouseMove={handleMouseMove}
       className="w-full h-dvh flex justify-center items-center bg-gray-100"
     >
-        {users.map((item, index) => (
-          <motion.div
-            key={`user-${index}`}
-            onMouseEnter={() => setActiveIndex(index)}
-            onMouseLeave={() => setActiveIndex(null)}
-            initial={{ x: index * -16 }}
-            animate={{ zIndex: activeIndex == index ? 50 : (index + 1) }}
-            className="w-14 h-14 flex justify-center relative"
-          >
-            <img 
-              className="w-full h-full rounded-full border-2 border-white"
-              src={item.image} 
-              alt="Avatar" 
-            />
-            <AnimatePresence>
-              {
-                activeIndex == index && (
-                  <motion.div 
-                    key={`tooltip-${index}`}
-                    initial={{ scale: .5, opacity: 0 }}
-                    animate={{ 
-                      scale: 1, 
-                      opacity: 1,
-                      rotate: prevX.current > currentX.current ? -10 : 10 
-                    }}
-                    exit={{ scale: .5, opacity: 0 }}
-                    transition={{ type: 'spring', duration: .7, bounce: .5 }}
-                    className="absolute -top-[120%] py-3 px-4 flex flex-col gap-2 justify-center items-center bg-black text-white rounded-lg"
-                  >
-                    <span className="leading-none text-nowrap font-medium">
-                      {item.name}
-                    </span>
-                    <span className="leading-none text-nowrap text-xs font-light">
-                      {item.jobTitle}
-                    </span>
-                  </motion.div>
-                )
-              }
+      <Credits />
+      {users.map((item, index) => (
+        <motion.div
+          key={`user-${index}`}
+          onMouseEnter={() => setActiveIndex(index)}
+          onMouseLeave={() => setActiveIndex(null)}
+          initial={{ x: index * -16 }}
+          animate={{ zIndex: activeIndex == index ? 50 : (index + 1) }}
+          className="w-14 h-14 flex justify-center relative"
+        >
+          <img 
+            className="w-full h-full rounded-full border-2 border-white"
+            src={item.image} 
+            alt="Avatar" 
+          />
+          <AnimatePresence>
+            {
+              activeIndex == index && (
+                <motion.div 
+                  key={`tooltip-${index}`}
+                  initial={{ scale: .5, opacity: 0 }}
+                  animate={{ 
+                    scale: 1, 
+                    opacity: 1,
+                    rotate: prevX.current > currentX.current ? -10 : 10 
+                  }}
+                  exit={{ scale: .5, opacity: 0 }}
+                  transition={{ type: 'spring', duration: .7, bounce: .5 }}
+                  className="absolute -top-[120%] py-3 px-4 flex flex-col gap-2 justify-center items-center bg-black text-white rounded-lg"
+                >
+                  <span className="leading-none text-nowrap font-medium">
+                    {item.name}
+                  </span>
+                  <span className="leading-none text-nowrap text-xs font-light">
+                    {item.jobTitle}
+                  </span>
+                </motion.div>
+              )
+            }
 
-            </AnimatePresence>
-          </motion.div>
-        ))}
+          </AnimatePresence>
+        </motion.div>
+      ))}
     </section>
   )
 }
---
+++ src/components/credits.tsx
@@ -0,0 +1,21 @@
+import { AnchorHTMLAttributes } from "react"
+
+export function Credits() {
+  return (
+    <section className="absolute top-6 left-0 w-full px-10 lg:px-20 flex flex-row justify-between">
+      <span>Coded by <Link href="https://x.com/Wilfried_Ng23">Wilfried Ngassarou</Link></span>
+    </section>
+  )
+}
+
+function Link(props: AnchorHTMLAttributes<HTMLAnchorElement>){
+  return (
+    <a 
+      {...props} 
+      target="_blank"
+      className="text-blue-500 font-medium underline"
+    >
+      {props.children}
+    </a>
+  )
+}
