ea0b0b5be0362181306f001ca5fd68b7f5720aa8
unknown <wilfriedngassarou23@gmail.com>
2025-01-25 12:09:04 +0100

    Added constant

---
+++ src/constants/users.ts
@@ -0,0 +1,32 @@
+export const users = [
+  {
+    name: 'Jeovani Ndjatchou',
+    jobTitle: 'Supply Chain Manager',
+    image: '/jeo.webp'
+  },
+  {
+    name: 'Wilfried Ngassarou',
+    jobTitle: 'Web Developer',
+    image: '/wilfried.webp'
+  },
+  {
+    name: 'Tony Pijeke',
+    jobTitle: 'UI/UX Designer',
+    image: '/john.webp'
+  },
+  {
+    name: 'Laka Honore',
+    jobTitle: 'SI Engineer',
+    image: '/honore.webp'
+  },
+  {
+    name: 'Divan N.',
+    jobTitle: 'Mobile Developer',
+    image: '/wilfried2.webp'
+  },
+  {
+    name: 'Tony Djeukam',
+    jobTitle: 'Financial Analyst',
+    image: '/djeukam.webp'
+  },
+]
