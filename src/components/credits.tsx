14e9fd1e5978e3f799b88faa6bb8de6f633110fe
unknown <wilfriedngassarou23@gmail.com>
2025-01-28 22:44:55 +0100

    Added credits(2)

--- src/components/credits.tsx
+++ src/components/credits.tsx
@@ -4,6 +4,7 @@ export function Credits() {
   return (
     <section className="absolute top-6 left-0 w-full px-10 lg:px-20 flex flex-row justify-between">
       <span>Coded by <Link href="https://x.com/Wilfried_Ng23">Wilfried Ngassarou</Link></span>
+      <span>Inspired by <Link href="https://x.com/aceternitylabs">Aceternity</Link></span>
     </section>
   )
 }
